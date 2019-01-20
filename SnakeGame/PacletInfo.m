e0f0b8703b2efcd0181ed8ab20c339aa6b30237f
miRoox <miroox@outlook.com>
2019-01-20 15:55:12 +0800

    paclet prototype

---
+++ SnakeGame/PacletInfo.m
@@ -0,0 +1,14 @@
+
+Palcet[
+  Name->"SnakeGame",
+  Version->"0.1.0",
+  Creator->"miRoox <miRoox@outlook.com>",
+  Description->"A Snake game.",
+  Extensions->{
+    {
+      "Kernel",
+      "Root"->".",
+      "Context"->{"SnakeGame`"}
+    }
+  }
+]
